e52d350c8d8498da42b211b7eca5b1bef5079e8b
Majood00 <majood00@ou.edu>
2021-07-13 14:13:30 -0500

    Some exercises from today.

---
+++ InClass_Exercises/Variables_Exercise_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Variables_Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my application! What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine(name + ", what is your monthly income?");
+            string response = Console.ReadLine();
+            double monthlyIncome = Convert.ToDouble(response);
+
+            Console.WriteLine(name + ", what is your monthly expenses?");
+            double monthlyExpense = Convert.ToDouble(Console.ReadLine());
+
+            double netIncome = monthlyIncome - monthlyExpense;
+
+            Console.WriteLine($"The difference between your income and expenses is {netIncome}.");
+
+        }
+    }
+}
---
+++ InClass_Exercises/Variables_Exercise_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Variables_Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a lower-bound number.");
+            string response = Console.ReadLine();
+            int lower = Convert.ToInt32(response);
+
+            Console.WriteLine("Please enter an upper-bound number.");
+            string response1 = Console.ReadLine();
+            int upper = Convert.ToInt32(response1);
+
+            Random rand = new Random();
+            int number = rand.Next(lower, upper);
+
+            Console.WriteLine(number.ToString("n0"));
+        }
+    }
+}
