a1550a35b574940eefacd4bfe8fb66ec47ae1ddb
Majood00 <majood00@ou.edu>
2021-07-13 14:15:31 -0500

    All exercises

---
+++ InClass_Exercises/Variables_Exercise_4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Variables_Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Beep(3700, 500);
+        }
+    }
+}
