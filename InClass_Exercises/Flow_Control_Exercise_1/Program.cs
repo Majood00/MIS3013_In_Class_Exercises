5e4da66f5b54587a38846396446277ce7732cc20
Majood00 <majood00@ou.edu>
2021-07-13 15:01:51 -0500

    new exercises

---
+++ InClass_Exercises/Flow_Control_Exercise_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Flow_Control_Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my application! What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine(name + ", what is your monthly income?");
+            string response = Console.ReadLine();
+            double monthlyIncome = Convert.ToDouble(response);
+
+            Console.WriteLine(name + ", what is your monthly expenses?");
+            double monthlyExpense = Convert.ToDouble(Console.ReadLine());
+
+            double netIncome = monthlyIncome - monthlyExpense;
+
+            if (netIncome >= 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+
+            Console.WriteLine($"The difference between your income and expenses is {netIncome.ToString("c3")}.");
+        }
+    }
+}
