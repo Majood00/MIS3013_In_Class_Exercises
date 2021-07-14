00c34d27aaa96d1d8dc17b088456ebb1bff23d6b
Majood00 <majood00@ou.edu>
2021-07-14 13:43:06 -0500

    solution for flow control exercise 2

---
+++ InClass_Exercises/Flow_Control_Exercise_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Flow_Control_Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int number = rand.Next(1, 101);
+
+            Console.WriteLine(number);
+
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                Console.WriteLine("Fizz Buzz");
+            }
+
+            else if (number % 3 == 0) 
+            {
+                Console.WriteLine("Fizz");
+            }
+
+            else if (number % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+
+            else
+            {
+                Console.WriteLine(number);
+            }
+
+        }
+    }
+}
