2cdede736bd6dce4e718cc2ad5b9c1dc959ff848
Majood00 <majood00@ou.edu>
2021-07-15 14:30:52 -0500

    created while loop reference and do while loop reference

---
+++ InClass_Exercises/Loops_Do/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Loops_Do
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            double sum = 0;
+            string answer;
+
+            do
+            {
+                Console.WriteLine("Enter your homework grade.");
+                double grade = Convert.ToDouble(Console.ReadLine()) / 100;
+
+                sum += grade;
+
+                counter++;
+                Console.WriteLine("Do you have another grade to enter? yes or no?");
+                answer = Console.ReadLine();
+            
+            } while (answer == "yes");
+
+            double average = sum / counter;
+
+            Console.WriteLine($"Your average score on homework is {average.ToString("P2")}");
+
+        }
+    }
+}
---
+++ InClass_Exercises/Loops_While_Loop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Loops_While_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your favorite #?"); ;
+            string input = Console.ReadLine();
+            int favoriteNumber;
+            bool isValidNumber = int.TryParse(input, out favoriteNumber);
+
+            while (int.TryParse(input, out favoriteNumber) == false)
+            {
+                Console.WriteLine($"{input} was not a valid integer. Please input a number.");
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("Thank you for telling us your favorite number.");
+
+        }
+    }
+}
