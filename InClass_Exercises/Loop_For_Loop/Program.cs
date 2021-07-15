f28130d153a6a919460df26c450cfa721ec3577d
Majood00 <majood00@ou.edu>
2021-07-15 14:50:54 -0500

    class exercises

---
+++ InClass_Exercises/Loop_For_Loop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Loop_For_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 2; i < 101; i = i +2)
+            {
+               Console.WriteLine(i);
+            }
+
+            int sum = 0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                sum = sum + i;
+            }
+
+            Console.WriteLine($"The summation from 1 to 100 is {sum.ToString("N0")}");
+        }
+    }
+}
--- InClass_Exercises/Loops_Do/Program.cs
+++ InClass_Exercises/Loops_Do/Program.cs
@@ -13,19 +13,28 @@ namespace Loops_Do
             do
             {
                 Console.WriteLine("Enter your homework grade.");
-                double grade = Convert.ToDouble(Console.ReadLine()) / 100;
+                double grade;
+                answer = Console.ReadLine();
+
+                while (double.TryParse(answer, out grade) == false)
+                {
+                    Console.WriteLine($"{answer} is an invalid grade. Enter your homework grade.");
+                    answer = Console.ReadLine();
+                }
 
                 sum += grade;
 
                 counter++;
+
                 Console.WriteLine("Do you have another grade to enter? yes or no?");
                 answer = Console.ReadLine();
-            
             } while (answer == "yes");
 
             double average = sum / counter;
 
-            Console.WriteLine($"Your average score on homework is {average.ToString("P2")}");
+            average /= 100;
+
+            Console.WriteLine($"Your average score on homework is {average.ToString("P2")}!");
 
         }
     }
