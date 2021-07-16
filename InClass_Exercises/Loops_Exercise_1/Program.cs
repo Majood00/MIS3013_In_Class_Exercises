edd71bca4e8149b81d3f21ea2f27828a3f94024f
Majood00 <majood00@ou.edu>
2021-07-16 13:32:22 -0500

    did exercise 1 for loops

--- InClass_Exercises/Loops_Exercise_1/Program.cs
+++ InClass_Exercises/Loops_Exercise_1/Program.cs
@@ -6,7 +6,42 @@ namespace Loops_Exercise_1
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("What would you like to count by?");
+            int whatToCountBy = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What number would you like to count up to?");
+            int upperBound = Convert.ToInt32(Console.ReadLine());
+
+            /* For Loop
+            for (int i = whatToCountBy; i <= upperBound; i = i + whatToCountBy)
+            {
+                if (i + whatToCountBy <= upperBound)
+                {
+                    Console.Write(i + ",");
+                }
+                else
+                {
+                    Console.Write(i);
+                }
+            }*/
+
+            // While Loop
+            int counter = whatToCountBy;
+            bool isFirstTimeThru = true;
+            while (counter <= upperBound)
+            {
+                if (isFirstTimeThru)
+                {
+                    Console.Write(counter);
+                    isFirstTimeThru = false;
+                }
+                else
+                {
+                    Console.Write(", " + counter);
+                }
+
+                counter = counter + whatToCountBy;
+            }
         }
     }
 }
