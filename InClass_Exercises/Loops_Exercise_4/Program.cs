b9b1bdcccc70fa55bf8234341372663e1fb56f7e
Majood00 <majood00@ou.edu>
2021-07-16 13:49:57 -0500

    done exercise 4 for loops

---
+++ InClass_Exercises/Loops_Exercise_4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Loops_Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "The quick brown fox jumps over the lazy dog";
+
+            // string sentence2 = "Hello";
+            // Console.WriteLine(sentence2.Length); // output: 5
+
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                Console.WriteLine(sentence[i]);
+            }
+        }
+    }
+}
