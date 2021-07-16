23f767f9a3db1798b44f06dd0c862dc7dff9043c
Majood00 <majood00@ou.edu>
2021-07-16 13:37:31 -0500

    completed exercise 2 for loops

---
+++ InClass_Exercises/Loops_Exercise_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Loops_Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What # do you want to count down from?");
+            int startingPoint;
+
+            if (int.TryParse(Console.ReadLine(), out startingPoint) == false)
+            {
+                Console.WriteLine("Sorry, that is invalid. Goodbye.");
+                Environment.Exit(0);
+            }
+
+            for (int i = startingPoint; i >= 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
