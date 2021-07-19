b315ff6e3e78dbfcd37b8e450d7ef516d32b8602
Majood00 <majood00@ou.edu>
2021-07-19 14:26:05 -0500

    strings example 1

--- InClass_Exercises/Strings_Example_1/Program.cs
+++ InClass_Exercises/Strings_Example_1/Program.cs
@@ -6,7 +6,36 @@ namespace Strings_Example_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string sentence = "The quick brown fox jumps over the lazy dog";
+
+            Console.WriteLine($"There are {sentence.Length.ToString("n0")} characters in the sentence.");
+
+            Console.WriteLine();
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (sentence[i] == ' ')
+                {
+                    Console.WriteLine();
+                    continue;
+                }   
+                Console.Write(sentence[i]);
+            }
+
+            Console.WriteLine("\nWhich letter do you want to know the count of?");
+            string letter = Console.ReadLine();
+            letter = letter.Trim();
+
+            Console.WriteLine();
+            int count = 0;
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (sentence[i] == Convert.ToChar(letter))
+                {
+                    count += 1;
+                }
+            }
+
+            Console.WriteLine($"There were {count} occurences of the letter '{letter}'");
         }
     }
 }
