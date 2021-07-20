b19ec8cd695b6fcddeccb95f572b2818ee5b079c
Majood00 <majood00@ou.edu>
2021-07-20 14:07:42 -0500

    finished collections example 1

--- InClass_Exercises/Collections_Example_1/Program.cs
+++ InClass_Exercises/Collections_Example_1/Program.cs
@@ -6,7 +6,24 @@ namespace Collections_Example_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string names = "Scooby Christina JENN clarke";
+
+            string name1 = "Jenn";
+            char[] name1AsArray = new char[4];
+            name1AsArray[0] = 'J';
+            name1AsArray[1] = 'e';
+            name1AsArray[2] = 'n';
+            name1AsArray[3] = 'n';
+
+            string[] seperatedNames = names.Split(' ');
+
+            for (int i = 0; i < seperatedNames.Length; i++)
+            {
+                string name = seperatedNames[i];
+                name = name.ToLower();
+                name = name.ToUpper()[0] + name.Substring(1);
+                Console.WriteLine(name);
+            }
         }
     }
 }
