247542e05ddfeb712c24d301c1059983ee490481
Majood00 <majood00@ou.edu>
2021-07-20 13:08:09 -0500

    created strings example 2

---
+++ InClass_Exercises/Strings_Example_2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Strings_Example_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string names = "Scooby Christina JENN clarke";
+
+
+        }
+    }
+}
