154776f6c0e496bb5f633299643ab8d547538218
Majood00 <majood00@ou.edu>
2021-07-21 14:06:13 -0500

    created collections array example

---
+++ InClass_Exercises/Collections_Array/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Collections_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstNames = new string[3];
+
+            firstNames[0] = "Darby";
+            firstNames[1] = "Shelby";
+            firstNames[2] = "Tim";
+
+            for (int i = 0; i < firstNames.Length; i++)
+            {
+                string firstName = firstNames[i];
+                Console.WriteLine(firstName);
+            }
+            
+        }
+    }
+}
