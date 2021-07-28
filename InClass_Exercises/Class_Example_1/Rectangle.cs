c3a51f7b5f78bf9fe696eb02b8edb6e2ed388a4c
Majood00 <majood00@ou.edu>
2021-07-28 14:43:39 -0500

    completed the Rectangle class problem

---
+++ InClass_Exercises/Class_Example_1/Rectangle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_Example_1
+{
+    public class Rectangle
+    {
+        public double Length { get; set; }
+
+        public double Width { get; set; }
+
+        // private double _Width;
+
+        /*public double GetWidth()
+        {
+            return _Width;
+        }
+
+        public void SetWidth(double w)
+        {
+            _Width = w;
+        }*/
+
+        public Rectangle()
+        {
+            Length = 0;
+            Width = 0;
+        }
+
+        public double CalculateArea()
+        {
+            double area = Length * Width;
+
+            return area;
+        }
+
+        public double CalculatePerimeter()
+        {
+            double perimeter = 2*(Length + Width);
+
+            return perimeter;
+        }
+
+    }
+}
