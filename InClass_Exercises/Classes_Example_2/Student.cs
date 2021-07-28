231010a7b753bf66ce022212cb142a121ffd6755
Majood00 <majood00@ou.edu>
2021-07-28 15:03:21 -0500

    created classes example 2

---
+++ InClass_Exercises/Classes_Example_2/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_Example_2
+{
+    public class Student
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string ClassTaken { get; set; }
+
+        public Student()
+        {
+            ID = 0;
+            Name = string.Empty;
+            ClassTaken = string.Empty;
+        }
+
+    }
+}
