0d155fcf6f4bce9313bf03a782e9f2a4a36c0bcf
Majood00 <majood00@ou.edu>
2021-07-29 13:16:34 -0500

    created classes example 3

---
+++ InClass_Exercises/Class_Example_3/Movie.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_Example_3
+{
+    class Movie
+    {
+        public string Title { get; set; }
+
+        public List<string> Genres { get; set; }
+
+        public int ReleaseYear { get; set; }
+
+        public Movie()
+        {
+            Title = string.Empty;
+
+            ReleaseYear = 0;
+
+            Genres = new List<string>;
+        }
+
+        public int Age()
+        {
+            DateTime today = DateTime.Now;
+
+            int age = today.Year - ReleaseYear;
+
+            return age;
+        }
+    }
+}
--- InClass_Exercises/Classes_Example_2/Student.cs
+++ InClass_Exercises/Classes_Example_2/Student.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Classes_Example_2
+namespace Class_Example_2
 {
     public class Student
     {
