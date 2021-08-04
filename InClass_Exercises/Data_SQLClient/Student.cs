375b8cba98a29a17df8904a4563e8c67fb7bb1c4
Majood00 <majood00@ou.edu>
2021-08-04 14:10:50 -0500

    did an in class example dealing with a database

---
+++ InClass_Exercises/Data_SQLClient/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_SQLClient
+{
+    class Student
+    {
+        public int StudentID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FavoriteColor { get; set; }
+
+        public Student()
+        {
+            StudentID = 0;
+
+            FirstName = string.Empty;
+
+            LastName = string.Empty;
+
+            FavoriteColor = string.Empty;
+        }
+
+        public override string ToString()
+        {
+            return $"{LastName}, {FirstName} ({StudentID}) likes {FavoriteColor}";
+        }
+    }
+}
